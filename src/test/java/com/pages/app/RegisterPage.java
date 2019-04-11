fa9445f79d5d7fbf75d9a09e5f5520b9a30a8b21
fernando <fer.nightwish86@gmail.com>
2019-04-11 17:08:34 -0300

    adding webelements to registerPage

--- src/test/java/com/pages/app/RegisterPage.java
+++ src/test/java/com/pages/app/RegisterPage.java
@@ -14,11 +14,43 @@ public class RegisterPage extends BasePage {
 		// TODO Auto-generated constructor stub
 	}
 	
-	@FindBy(tagName="title")
-	WebElement title;
-	
 	@FindBy(id="CONTROLGROUPREGISTERVIEW_ButtonSubmit")
 	WebElement submitButton;
+	
+	@FindBy(id="CONTROLGROUPREGISTERVIEW_PersonInputRegisterView_TextBoxFirstName")
+	WebElement firstNameInput;
+	
+	@FindBy(id="CONTROLGROUPREGISTERVIEW_PersonInputRegisterView_TextBoxLastName")
+	WebElement lastNameInput;
+	
+	@FindBy(xpath="//div[@class='flag-container']")
+	WebElement flagList;
+	
+	@FindBy(id="CONTROLGROUPREGISTERVIEW_MemberInputRegisterView_PasswordFieldAgentPassword")
+	WebElement passwordInput;
+	
+	@FindBy(id="CONTROLGROUPREGISTERVIEW_MemberInputRegisterView_PasswordFieldPasswordConfirm")
+	WebElement confirmPasswordInput;
+	
+	@FindBy(id="CONTROLGROUPREGISTERVIEW_PersonInputRegisterView_DropDownListDOBDay")
+	WebElement dayDropList;
+	
+	@FindBy(id="CONTROLGROUPREGISTERVIEW_PersonInputRegisterView_DropDownListDOBMonth")
+	WebElement monthDropList;
+	
+	@FindBy(id="CONTROLGROUPREGISTERVIEW_PersonInputRegisterView_DropDownListDOBYear")
+	WebElement yearDropList;
+	
+	@FindBy(id="CONTROLGROUPREGISTERVIEW_PersonInputRegisterView_TextBoxEmail")
+	WebElement emailInput;
+	
+	@FindBy(id="CONTROLGROUPREGISTERVIEW_PersonInputRegisterView_TEXTBOXPANCARD")
+	WebElement cardNumberInput;
+	
+	@FindBy(id="chkSpiceClubTnC")
+	WebElement checkbox;
+	
+
 	
 	public String getPageTitle() {
 		return getDriver().getTitle();
